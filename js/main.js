bfa3631b34701742964e6efc295b21b7cab3e594
dimassspb <60599937+dimassspb@users.noreply.github.com>
2021-03-31 17:55:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+//слайдер
+const swiper = new Swiper('.swiper-container', { 
+    el: '.projects-pagination',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true,
+  });
+
